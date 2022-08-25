df3e1f6e0ada8120c66be12f9985cb973719a9d8
codybronsell <110582217+codybronsell@users.noreply.github.com>
2022-08-25 00:04:23 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,29 +68,46 @@ var fight = function(enemyName) {
     }
   } // end of while loop
 }; // end of fight function
-
-// fight each enemy-robot by looping over them and fighting them one at a time
-for (var i = 0; i < enemyNames.length; i++) {
-  // if player is still alive, keep fighting
+var startGame = function() {
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      
+      var pickedEnemyName = enemyNames[i];
+      
+      enemyHealth = 50;
+      
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert('You have lost your robot in battle! Game Over!');
+      break;
+    }
+  }
+  endGame();
+};
+var endGame = function() {
   if (playerHealth > 0) {
-    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
-    // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
-
-    // reset enemyHealth before starting new fight
-    enemyHealth = 50;
-
-    // use debugger to pause script from running and check what's going on at that moment in the code
-    // debugger;
-
-    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedEnemyName);
+    window.alert("Great job, you've survived the game! You now have the score of " + playerMoney + ". ");
   }
-  // if player isn't alive, stop the game
   else {
-    window.alert('You have lost your robot in battle! Game Over!');
-    break;
+    window.alert("you've lost your robot in battle.");
   }
-}
+
+  var playAgainConfirm = window.confirm("would you like to play agian?");
+
+  if (playAgainConfirm) {
+    startGame();
+  }
+  else {
+    window.alert("THay you for playing robot gladiators! come back soon!");
+  }
+
+  
+  window.alert("the game has now ended. Let's see how you did!");
+};
+
+startGame();
